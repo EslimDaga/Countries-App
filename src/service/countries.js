5bd54020beddbec28e2ef3686a1f533ee068c5d0
Eslim Daga <esliim9811@gmail.com>
2022-01-09 16:34:57 -0500

    Add get countries api

---
+++ src/service/countries.js
@@ -0,0 +1,9 @@
+import Axios from "axios";
+
+const url = "https://restcountries.com/v3.1/all";
+
+export const countries = async() => {
+  const response = await Axios.get(url);
+  const countries = response.data;
+  return countries;
+}
