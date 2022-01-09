f92b5236b34077d073a88c5d97ea1c109f72336a
Eslim Daga <esliim9811@gmail.com>
2022-01-09 17:44:40 -0500

    Finish print all countries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
+import { countries } from "./service/countries";
+import styled from "styled-components";
+
+const ImageFlag = styled.img`
+  width: 100%;
+  height: 200px;
+`
 
 const App = () => {
+
+  const [allCountries, setAllCountries] = useState([])
+
+  useEffect(() => {
+    countries().then((countries) => {
+      setAllCountries(countries);
+    })
+  },[])
+
   return (
     <>
       <Header />
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-center">
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-            <div class="card card-bordered">
-              <figure>
-                <img src="https://picsum.photos/id/1005/400/250" alt="pictur"/>
-              </figure>
-              <div class="card-body">
-                <h2 class="card-title">Top image
-                  <div class="badge mx-2 badge-secondary">NEW</div>
-                </h2>
-                <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
-                <div class="justify-end card-actions">
-                  <button class="btn btn-secondary">More info</button>
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-5">
+            {
+              allCountries.map((countrie) => (
+                <div className="card card-bordered" key={countrie.alpha2Code}>
+                  <figure>
+                    <ImageFlag src={countrie.flags.png} alt={countrie.name} width="auto" height="200"/>
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{countrie.name}
+                      <div className="badge mx-2 badge-primary-focus">{countrie.alpha2Code}</div>
+                    </h2>
+                    <div className="flex justify-between">
+                      <h2>Nombre Nativo</h2>
+                      <h2 className="font-bold">{countrie.nativeName}</h2>
+                    </div>
+                    <div className="flex justify-between">
+                      <h2>Población</h2>
+                      <h2 className="font-bold">{countrie.population}</h2>
+                    </div>
+                    <div className="flex justify-between">
+                      <h2>Capital</h2>
+                      <h2 className="font-bold">{countrie.capital}</h2>
+                    </div>
+                    <div className="flex justify-between">
+                      <h2>Región</h2>
+                      <h2 className="font-bold">{countrie.region}</h2>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              ))
+            }
           </div>
         </div>
       </div>
