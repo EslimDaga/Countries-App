33f4cde3715d8362e169589c6db78e9bb86d5607
Eslim Daga <esliim9811@gmail.com>
2022-01-09 17:45:06 -0500

    Change endpoint

--- src/service/countries.js
+++ src/service/countries.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const url = "https://restcountries.com/v3.1/all";
+const url = "https://restcountries.com/v2/all";
 
 export const countries = async() => {
   const response = await Axios.get(url);
